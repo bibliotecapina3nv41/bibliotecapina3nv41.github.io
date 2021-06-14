7d5d0fa5712f63142b3edb40cc605f472cd99ce1
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-14 17:36:22 -0500

    Autenticacion

--- js/Ctrl.js
+++ js/Ctrl.js
@@ -1,1 +1,9 @@
-
+firebase.initializeApp({
+    apiKey: "AIzaSyAmbBjNaFREaF89rFg71sJHd9x4NwdPXEY",
+    authDomain: "bibliotecapina3nv41-af2a9.firebaseapp.com",
+    projectId: "bibliotecapina3nv41-af2a9",
+    storageBucket: "bibliotecapina3nv41-af2a9.appspot.com",
+    messagingSenderId: "295662283806",
+    appId: "1:295662283806:web:a0585d34bff286e846520f",
+    measurementId: "G-1F3GLE5CZV"
+  });
