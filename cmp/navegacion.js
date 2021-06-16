162a7fc302cd385ef324fe6a4fdc3e447613cc38
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-16 17:10:25 -0500

    refrol

--- cmp/navegacion.js
+++ cmp/navegacion.js
@@ -1,6 +1,8 @@
 const firestore = firebase.firestore();
-const refCol = firestore.collection("Rol");
-const refCol = firestore.collection("Usuario");
+const refRol = firestore.collection("Rol");
+const refUsr = firestore.collection("Usuario");
+const auth = firebase.auth();
+
 
 class Navegacion extends HTMLElement {
     connectedCallback() {
@@ -13,25 +15,14 @@ class Navegacion extends HTMLElement {
         </ul>`;
       this.ul =
         this.querySelector("ul");
-      getAuth().onAuthStateChanged(
+        auth.onAuthStateChanged(
         usuario => this.
           cambiaUsuario(usuario),
         muestraError);
-    }
+    //}
   
-    /**
-     * @param {import(
-        "../lib/tiposFire.js").User}
-        usu */
-    async cambiaUsuario(usu) {
-      if (usu && usu.email) {
-        let html = "";
-        const roles =
-          await cargaRoles(
-            usu.email);
-        /* Enlaces para solo
-         * para clientes. */
-        if (roles.has("Ugratuito")) {
+
+        if (refRol.has("Ugratuito")) {
           html += /* html */
             `<li>
               <a href="gratuitos.html">Libros gratis</a>
@@ -40,7 +31,7 @@ class Navegacion extends HTMLElement {
         /* Enlaces para solo
          * administradores.
          */
-        if (roles.has(
+        if (refRol.has(
           "Administrador")) {
           html += /* html */
             `<li>
@@ -51,9 +42,7 @@ class Navegacion extends HTMLElement {
             </li>`;
         }
         this.ul.innerHTML += html;
-      }
     }
-  }
-  
+}
   customElements.define(
     "navegacion", Navegacion);
