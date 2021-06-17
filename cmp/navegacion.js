b1af3a42be6f728b4098ddfcb736bcc6e38f2000
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-17 03:40:56 -0500

    validausr

--- cmp/navegacion.js
+++ cmp/navegacion.js
@@ -1,7 +1,7 @@
 const firestore = firebase.firestore();
 const refUsr = firestore.collection("Usuario");
 const refRol = firestore.collection("Rol");
-const correo = firebase.auth().onAuthStateChanged(usuario => usuario.email);
+//const correo = firebase.auth().onAuthStateChanged(usuario => usuario.email);
 
   
   class Navegacion extends HTMLElement {
@@ -15,26 +15,34 @@ const correo = firebase.auth().onAuthStateChanged(usuario => usuario.email);
         </ul>`;
         this.ul = this.querySelector("ul");
         //const auth = firebase.auth();
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            let html = "";
-            const roles = await cargaRoles(correo);
-            if (roles.has("Administrador")) {
-              html += /* html */
-                `<li>
-                  <a href=
-                    "gratuitos.html">Gratuitos</a>
-                </li>`;
-            }
-            this.ul.innerHTML += html;
-            }
+        getAuth().onAuthStateChanged(usuario => this.cambiaUsuario(usuario), muestraError);
+        }
+      }
 
-           else {
-            console.error();
+      async function cambiaUsuario(usu) {
+        if (usu && usu.email) {
+          let html = "";
+          const roles = await cargaRoles(usu.email);
+          /* Enlaces para solo
+           * para clientes. */
+          if (roles.has("Cliente")) {
+            html += /* html */
+              `<li>
+                <a href="gratuitos.html">Gratuitos</a>
+              </li>`;
           }
-        });
-    }
-  }
+          /* Enlaces para solo
+           * administradores.
+           */
+          if (roles.has("Administrador")) {
+            html += /* html */
+              `<li>
+                <a href="miembros.html">Miembros</a>
+              </li>`;
+          }
+          this.ul.innerHTML += html;
+        }
+      }
 
   async function cargaRoles(mail){
     const roles = await refUsr.doc(mail).get();
