2bb0ba3d6e78f195a7a2004ba6107e0e01d64299
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-16 20:19:52 -0500

    pruebausr

--- cmp/navegacion.js
+++ cmp/navegacion.js
@@ -1,12 +1,7 @@
-import {
-    cargaRoles
-  } from "../js/datosesion.js";
-  import {
-    getAuth
-  } from "../lib/instancias.js";
-  import {
-    muestraError
-  } from "../lib/util.js";
+const firestore = firebase.firestore();
+const refUsr = firestore.collection("Usuario");
+const refCol = firestore.collection("Rol");
+const auth = firebase.auth();
   
   class Navegacion extends HTMLElement {
     connectedCallback() {
@@ -17,52 +12,20 @@ import {
               Sesión</a>
           </li>
         </ul>`;
-      this.ul =
-        this.querySelector("ul");
-      getAuth().onAuthStateChanged(
-        usuario => this.
-          cambiaUsuario(usuario),
-        muestraError);
-    }
-  
-    /**
-     * @param {import(
-        "../lib/tiposFire.js").User}
-        usu */
-    async cambiaUsuario(usu) {
-      if (usu && usu.email) {
-        let html = "";
-        const roles =
-          await cargaRoles(
-            usu.email);
-        /* Enlaces para solo
-         * para clientes. */
-        if (roles.has("Cliente")) {
-          html += /* html */
+      
+        if(refUsr.has("Administrador")){
+            html += /* html */
             `<li>
-              <a href=
-                "chat.html">Chat</a>
-            </li>`;
-        }
-        /* Enlaces para solo
-         * administradores.
-         */
-        if (roles.has(
-          "Administrador")) {
-          html += /* html */
-            `<li>
-              <a href=
-  "pasatiempos.html">Pasatiempos</a>
+              <a href="gratuitos.html">Pasatiempos</a>
             </li>
             <li>
               <a href=
-        "usuarios.html">Usuarios</a>
+                "miembros.html">Usuarios</a>
             </li>`;
         }
-        this.ul.innerHTML += html;
       }
     }
-  }
+  
   
   customElements.define(
     "navegacion", Navegacion);
