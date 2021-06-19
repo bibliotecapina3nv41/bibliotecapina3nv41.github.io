48be6376704089c498c6f43b64ace87b39f2cc3f
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-19 14:50:13 -0500

    CargaFinal

--- js/CtrlLibronuevo.js
+++ js/CtrlLibronuevo.js
@@ -9,20 +9,10 @@
   const storage = firebase.storage();
   const forma = document["forma"];
   
-  //firebase.auth().onAuthStateChanged(valida, muestraError);
-  
-
-  
-//async function valida(usuario){
-    //  if(usuario && usuario.email){
-         forma.addEventListener("submit", guarda); //
-      
-    
-  
+ forma.addEventListener("submit", guarda);
     
  async function guarda(evt){
-
-   // try{
+    try{
         evt.preventDefault();
         const formData = new FormData(forma);
         const nombre = getString(formData, "nomLibr").trim();
@@ -40,23 +30,9 @@
         };
         await storage.ref(nombre).put(Sarch, metadata);
         muestraMiembros();
-  /*  } catch{
+      } catch {
         muestraError(e);
-   }*/
+      }
+
 }
-/*}else{
-  muestraError(e);
-}*/
-//}
-/*
-async function sube(){
-    try{
-        const formData = new FormData(forma);
-        const nombrelib = getString(formData, "nomLibr").trim();
-        const libroCarg = formData.get("libroCarga");
-        await storage.ref(nombrelib).put(libroCarg);    
-        
-    } catch{
-        muestraError(e);
-    }
-}*/
+
