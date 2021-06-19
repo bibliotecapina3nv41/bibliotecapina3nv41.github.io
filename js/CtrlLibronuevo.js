48d91c6000b7aafebd175c77b7833d4c860998da
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-19 01:16:33 -0500

    carga libro

---
+++ js/CtrlLibronuevo.js
@@ -0,0 +1,39 @@
+  import {
+    getString,
+    muestraError
+  } from "../lib/util.js";
+
+  const docRef = firestore.collection("Libros");
+  const sto = firebase.storage();
+  const forma = document["forma"];
+  forma.addEventListener("submit", guarda);
+
+async function guarda(){
+    try{
+        const formData = new FormData(forma);
+        const id = getString(formData, nomLibr).trim();
+        const aut = getString(formData, autor).trim();
+        const data = {
+            id,
+            aut
+        }
+        await docRef.doc(id).set(data);
+        const libroC = formData.get("libroCarga");
+        await subeStorage(id, libroC);
+        muestraMiembros();
+    } catch{
+        muestraError(e);
+    }
+}
+
+async function subeStorage(nombre, archivo) {
+  if (archivo instanceof File && archivo.size > 0) {
+    await sto.ref(nombre).put(archivo);
+  }
+}
+
+function muestraMiembros() {
+  location.href = "miembros.html";
+}
+
+
