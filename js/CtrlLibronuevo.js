d9dfde8d1c82fbd2b3d2014972f66ec4bed5f7cd
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-19 16:27:16 -0500

    cargaStoFin

--- js/CtrlLibronuevo.js
+++ js/CtrlLibronuevo.js
@@ -8,13 +8,14 @@
   const refLib = firestore.collection("Libros");
   const storage = firebase.storage();
   const forma = document["forma"];
-  var uploader = document.getElementById('uploader');
+  var estatus = document.getElementById("estatus")
   
  forma.addEventListener("submit", guarda);
     
  async function guarda(evt){
     try{
         evt.preventDefault();
+        estatus.textContent = "Espere...";
         const formData = new FormData(forma);
         const nombre = getString(formData, "nomLibr").trim();
         console.log(nombre);
@@ -24,18 +25,13 @@
             nombre,
             aut
         };
+        
         await refLib.doc(nombre).set(data);
         const Sarch = formData.get("libroCarga");
         var metadata = {
           contentType: 'application/pdf',
         };
-        var task = await storage.ref(nombre).put(Sarch, metadata);
-        task.on('state_changed',
-          function progress(snapshot){
-            var percentage = (snapshot.bytesTransferred /
-                snapshot.totalBytes) * 100;
-                uploader.value = percentage;
-          });
+        await storage.ref(nombre).put(Sarch, metadata);
         muestraMiembros();
       } catch {
         muestraError(e);
