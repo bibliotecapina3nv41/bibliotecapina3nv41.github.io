807587ec170da84356c9df4c56b5ebcec7a3ceab
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-19 21:36:36 -0500

    pruebadesp7

--- js/CtrlGratuitos.js
+++ js/CtrlGratuitos.js
@@ -8,34 +8,25 @@ const refLib = firestore.collection("Libros");
 const storage = firebase.storage();
 const lista = document.querySelector("#lista");
 
-getAuth().onAuthStateChanged(consulta, muestraError);
 
-function consulta(){
 refLib.onSnapshot(
     snapshot => {
       snapshot.forEach(doc => {
           let html = "";
-          if(snapshot.size > 0){
             const datos = doc.data();
             const nombre = cod(datos.nombre);
             const autor = cod(datos.aut);
             const enlace = await storage.ref(nombre).getDownloadURL();
             html += /* html */
             `<li>
-                <a class="fila" href="${cod(enlace)}">
+                <a class="fila" href="${enlace}">
                     <span class="texto">
                     <strong class="primario">${cod(doc.id)}<br>${cod(autor)}</strong>
                     </span>
                 </a>
             </li>`;
-        } else {
-            html += /* html */
-                `<li class="vacio">
-                    -- No se encontraron archivos --
-                </li>`;
-        }
+        
          lista.innerHTML = html;
       })
     },
     error => console.error(error));
-}
