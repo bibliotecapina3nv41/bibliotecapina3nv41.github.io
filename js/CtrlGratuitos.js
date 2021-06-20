9b9498e11554df682af258bef140843a29b40299
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-20 00:57:53 -0500

    pruebaConsultaP

--- js/CtrlGratuitos.js
+++ js/CtrlGratuitos.js
@@ -8,25 +8,51 @@ const refLib = firestore.collection("Libros");
 const storage = firebase.storage();
 const lista = document.querySelector("#lista");
 
+getAuth().onAuthStateChanged(protege, muestraError);
 
-refLib.onSnapshot(
-    snapshot => {
-      snapshot.forEach(doc => {
-          let html = "";
-            const datos = doc.data();
-            const nombre = cod(datos.nombre);
-            const autor = cod(datos.aut);
-            const enlace = storage.ref(nombre).getDownloadURL();
-            html += /* html */
-            `<li>
-                <a class="fila" href="${cod(enlace)}">
-                    <span class="texto">
-                    <strong class="primario">${cod(doc.id)}<br>${autor}</strong>
-                    </span>
-                </a>
-            </li>`;
-        
-         lista.innerHTML = html;
-      })
-    },
-    error => console.error(error));
+async function protege(usuario) {
+    if (usuario && usuario.email) {
+      consulta();
+    }
+  }
+
+  function consulta() {
+    refLib.onSnapshot(htmlLista, errConsulta);
+  }
+
+
+  async function htmlLista(snap) {
+    let html = "";
+    if (snap.size > 0) {
+      let libros = [];
+      snap.forEach(doc => libros.push(htmlFila(doc)));
+      const htmlFilas = await Promise.all(libros);
+      html += htmlFilas.join("");
+    } else {
+      html += /* html */
+        `<li class="vacio">
+          -- No se encontraron archivos. --
+        </li>`;
+    }
+    lista.innerHTML = html;
+  }
+
+  async function htmlFila(doc) {
+    const datos = doc.data();
+    const nombre = cod(datos.nombre);
+    const autor = cod(datos.aut);
+    const enlace = cod(await storage.ref(nombre).getDownloadURL());
+    return (/* html */
+        `<li>
+        <a class="fila" href="${cod(enlace)}">
+            <span class="texto">
+            <strong class="primario">${cod(doc.id)}<br>${cod(autor)}</strong>
+            </span>
+        </a>
+    </li>`);
+  }
+
+  function errConsulta(e) {
+    muestraError(e);
+    consulta();
+  }
