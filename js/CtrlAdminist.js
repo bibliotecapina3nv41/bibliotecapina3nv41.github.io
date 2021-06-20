9399e0b6b96071311c1ec7b5ea5faff0de08046d
bibliotecapina3nv41 <bibliotecapina3nv41@gmail.com>
2021-06-20 12:10:32 -0500

    PruebaAdmin

--- js/CtrlAdminist.js
+++ js/CtrlAdminist.js
@@ -1,5 +1,28 @@
+import {
+    getString,
+    muestraError,
+  } from "../lib/util.js";
+
+
 const firestore = firebase.firestore();
   const refLib = firestore.collection("Libros");
   const storage = firebase.storage();
   const forma = document["forma"];
   var estatus = document.getElementById("estatus");
+
+  forma.addEventListener("submit", elimina);
+
+  async function elimina() {
+    try {
+      if (confirm("Confirmar la " + "eliminación")) {
+        const formData = new FormData(forma);
+        const nombre = getString(formData, "nomLibr").trim();
+        estatus.textContent = "Eliminando...";
+        await refLib.doc(nombre).delete();
+        await storage.ref(nombre).delete();
+      }
+    } catch (e) {
+      muestraError(e);
+    }
+  }
+
